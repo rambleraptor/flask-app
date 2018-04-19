323bb4ca413bd9b2c5cdce618f6ded71cc612d4c
Alex Stephen <alexstephen@google.com>
2018-04-19 11:35:24 -0700

    adding hostname + license

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+import socket
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/hostname")
+def hostname():
+    return socket.gethostname()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
